7e94d28377cb214d286af431007ff378dc959346
AMCT <AMCT@DESKTOP-Q0U9DVJ>
2021-11-03 22:23:22 +0200

    add project

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
@@ -10,8 +9,7 @@ import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
-
-
+import { ProjectsComponent } from './projects/projects.component';
 
 
 @NgModule({
@@ -21,6 +19,7 @@ import { ContactComponent } from './contact/contact.component';
     NavbarComponent,
     FooterComponent,
     ContactComponent,
+    ProjectsComponent,
     
 
  
